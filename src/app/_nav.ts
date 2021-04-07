919c1473fb8243973f3e75178969aadd826d8423
HybaKelmy <hyba.kelmi@gmail.com>
2021-04-07 15:28:19 +0100

    first commit

---
+++ src/app/_nav.ts
@@ -0,0 +1,65 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    
+  },
+
+  {
+    name: 'Add an office',
+    url: '/theme/colors',
+    icon: 'icon-plus'
+  },
+  {
+    name: 'Add a user',
+    url: '/theme/typography',
+    icon: 'icon-plus'
+  },
+ 
+      {
+        name: 'Listing doctors',
+        url: '/base/cards',
+        icon: 'icon-list'
+      },
+      {
+        name: 'Listing secretaries',
+        url: '/base/carousels',
+        icon: 'icon-list'
+      },
+     
+    
+    
+ 
+    
+  
+  {
+    name: 'Write a note',
+    url: '/icons/flags',
+    icon: 'icon-note'
+  },
+  
+ 
+
+
+    
+  
+  {
+   
+  },
+ 
+
+
+      {
+        name: 'Login',
+        url: '/login',
+        icon: 'icon-star'
+      },
+  
+  
+
+
+
+];
