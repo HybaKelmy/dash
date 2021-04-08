9c188c7d690d4f16c6548c3de5b0e06a6b4f7c68
HybaKelmy <hyba.kelmi@gmail.com>
2021-04-08 16:45:38 +0100

    fir commit

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,18 +1,32 @@
 import {Component} from '@angular/core';
 import { navItems } from '../../_nav';
 
+import {ViewChild} from '@angular/core';
+import {ModalDirective} from 'ngx-bootstrap/modal';
+
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './default-layout.component.html'
+  
 })
 export class DefaultLayoutComponent {
   public sidebarMinimized = false;
   public navItems = navItems;
-
+  @ViewChild('myModal') public myModal: ModalDirective;
+  @ViewChild('largeModal') public largeModal: ModalDirective;
+  @ViewChild('smallModal') public smallModal: ModalDirective;
+  @ViewChild('primaryModal') public primaryModal: ModalDirective;
+  @ViewChild('successModal') public successModal: ModalDirective;
+  @ViewChild('warningModal') public warningModal: ModalDirective;
+  @ViewChild('dangerModal') public dangerModal: ModalDirective;
+  @ViewChild('infoModal') public infoModal: ModalDirective;
   toggleMinimize(e) {
     this.sidebarMinimized = e;
   }
 }
+
+
 
 
 
